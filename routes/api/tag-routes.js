28322bab5fa774078e8236ae7fc77fe4e4072358
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-11-03 21:45:09 -0700

    Added get and post req to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,18 +3,39 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const getTag = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag }] 
+    });
+    res.status(200).json(getTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const getTagId = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }] 
+    });
+    if(!getTagId){
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+    res.status(200).json(getTagId);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const postTag = await Tag.create(req.body);
+    res.status(200).json(postTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {

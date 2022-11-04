4fdfafd065be441a7909f2d908ec9a6fc59d566b
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-11-03 22:01:44 -0700

    Completed tag-Routes.js file's routes.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,12 +38,38 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    let putTagId = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!putTagId[0]) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(putTagId);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    let deleteTagId = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deleteTagId) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+    res.status(200).json(deleteTagId);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
